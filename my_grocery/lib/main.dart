8c5ba0683181fa691dd8f0a50c32f89bb59b6d5c
Brian Blaze <brianblaze14@outlook.com>
2024-12-26 12:08:40 +0000

    addition of the app theme

--- my_grocery/lib/main.dart
+++ my_grocery/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_grocery/route/app_page.dart';
 import 'package:my_grocery/route/app_route.dart';
+import 'package:my_grocery/web/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +17,9 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       getPages: AppPage.list,
       initialRoute: AppRoute.dashboard,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
