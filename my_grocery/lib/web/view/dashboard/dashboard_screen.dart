ca4962ba8ec754a63700091cdf91678638254c80
Brian Blaze <brianblaze14@outlook.com>
2024-12-26 14:31:41 +0000

    addition of the index selection

--- my_grocery/lib/web/view/dashboard/dashboard_screen.dart
+++ my_grocery/lib/web/view/dashboard/dashboard_screen.dart
@@ -1,60 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
+import 'package:get/get.dart';
+import 'package:my_grocery/web/controller/dashboard_controller.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
+    return GetBuilder<DashboardController>(
+      builder: (controller) => Scaffold(
+        body: SafeArea(
           child: IndexedStack(
-        index: 0,
-        children: [
-          Container(),
-          Container(color: Colors.red),
-          Container(color: Colors.blue),
-          Container(color: Colors.amber),
-        ],
-      )),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border(
-            top: BorderSide(
-              color: Theme.of(context).colorScheme.secondary,
-              width: 0.7,
-            ),
+            index: controller.tabIndex,
+            children: [
+              Container(),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.blue,
+              ),
+              Container(
+                color: Colors.amber,
+              ),
+            ],
           ),
         ),
-        child: SnakeNavigationBar.color(
-          behaviour: SnakeBarBehaviour.floating,
-          snakeShape: SnakeShape.circle,
-          padding: const EdgeInsets.symmetric(vertical: 5),
-          unselectedLabelStyle: const TextStyle(fontSize: 11),
-          snakeViewColor: Theme.of(context).primaryColor,
-          unselectedItemColor: Theme.of(context).colorScheme.secondary,
-          showSelectedLabels: true,
-          currentIndex: 0,
-          onTap: (val) {},
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              top: BorderSide(
+                color: Theme.of(context).colorScheme.secondary,
+                width: 0.7,
+              ),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.category),
-              label: 'Category',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.menu),
-              label: 'Menu',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'Account',
-            ),
-          ],
+          ),
+          child: SnakeNavigationBar.color(
+            behaviour: SnakeBarBehaviour.floating,
+            snakeShape: SnakeShape.circle,
+            padding: const EdgeInsets.symmetric(vertical: 5),
+            unselectedLabelStyle: const TextStyle(fontSize: 11),
+            snakeViewColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Theme.of(context).colorScheme.secondary,
+            showSelectedLabels: true,
+            currentIndex: controller.tabIndex,
+            onTap: (val) {
+              controller.updateIndex(val);
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.category),
+                label: 'Category',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.menu),
+                label: 'Menu',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: 'Account',
+              ),
+            ],
+          ),
         ),
       ),
     );
