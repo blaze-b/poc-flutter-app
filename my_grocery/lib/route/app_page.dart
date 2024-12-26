244f05fb8e7a97c024da6fbcbb2326c44554682c
Brian Blaze <brianblaze14@outlook.com>
2024-12-26 11:50:08 +0000

    adding the dashboard app route changes

---
+++ my_grocery/lib/route/app_page.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:my_grocery/route/app_route.dart';
+import 'package:my_grocery/web/view/dashboard/dashboard_screen.dart';
+
+class AppPage {
+  static var list = [
+    GetPage(name: AppRoute.dashboard, page: () => const DashboardScreen())
+  ];
+}
