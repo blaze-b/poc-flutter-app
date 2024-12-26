4225546c295299b6467a2167d68d4f6b500cceab
Brian Blaze <brianblaze14@outlook.com>
2024-12-26 20:17:42 +0000

    addition of the badge for testing

--- my_grocery/lib/web/component/main_header.dart
+++ my_grocery/lib/web/component/main_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
 
 class MainHeader extends StatelessWidget {
   const MainHeader({super.key});
@@ -48,9 +49,7 @@ class MainHeader extends StatelessWidget {
               ),
             ),
           ),
-          const SizedBox(
-            width: 10,
-          ),
+          const SizedBox(width: 10),
           Container(
             decoration: BoxDecoration(
                 color: Colors.white,
@@ -65,6 +64,31 @@ class MainHeader extends StatelessWidget {
             width: 46,
             padding: const EdgeInsets.all(12),
             child: const Icon(Icons.filter_alt_outlined),
+          ),
+          const SizedBox(width: 10),
+          badges.Badge(
+            badgeContent: const Text(
+              "1",
+              style: TextStyle(color: Colors.white),
+            ),
+            badgeStyle: badges.BadgeStyle(
+              badgeColor: Theme.of(context).primaryColor,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.6),
+                      blurRadius: 8,
+                    )
+                  ]),
+              height: 46,
+              width: 46,
+              padding: const EdgeInsets.all(12),
+              child: const Icon(Icons.shopping_cart_checkout_outlined),
+            ),
           ),
         ],
       ),
